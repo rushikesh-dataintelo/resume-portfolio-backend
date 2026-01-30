193c27ae5b0bd402dad3ece3785ddad62d7176fd
Rushi788816 <rushikeshsonar348@gmail.com>
2026-01-30 15:43:21 +0530

    cors php file changes

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,11 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'https://resume-portfolio-platform.vercel.app',
+        'https://resume-portfolio-platform-oqiiej0s3-rushikesh-works-projects.vercel.app',
     ],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-    'supports_credentials' => true,
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
 ];
